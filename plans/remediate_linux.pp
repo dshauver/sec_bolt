7597355e928eae02d89c68ba6a85cae7af7ada16
Dan Shauver <dan.shauver@puppet.com>
2019-06-25 10:32:53 -0500

    Adding kernel checks

--- plans/remediate_linux.pp
+++ plans/remediate_linux.pp
@@ -1,7 +1,10 @@
 plan sec_bolt::remediate_linux(
   TargetSpec $nodes,
 ) {
-    run_task('sec_bolt::disable_nix_user', $nodes)
-    run_task('sec_bolt::remove_admin_nix', $nodes)
-    run_command('yum remove telnet -y', $nodes)
+    run_plan(facts, nodes => $nodes)
+    $centos_nodes = get_targets($nodes).filter |$n| { $n.facts['os']['name'] == 'CentOS'
+
+    run_task('sec_bolt::disable_nix_user', $centos_nodes)
+    run_task('sec_bolt::remove_admin_nix', $centos_nodes)
+    run_command('yum remove telnet -y', $centos_nodes)
 }
