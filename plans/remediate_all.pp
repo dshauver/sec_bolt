9501c25680c2c2cd354d61c51f4ee039c8c0d4a5
Dan Shauver <dan.shauver@puppet.com>
2019-06-25 13:34:50 -0500

    fixing remediate plan

--- plans/remediate_all.pp
+++ plans/remediate_all.pp
@@ -1,6 +1,5 @@
 plan sec_bolt::remediate_all(
   TargetSpec $nodes,
-  Optional[String[1]] $disable_user = undef,
 ) {
   # This collects facts on nodes and update the inventory
   run_plan(facts, nodes => $nodes)
@@ -21,7 +20,7 @@ plan sec_bolt::remediate_all(
 
 #Windows
   #Disable DPRoberts User
-  run_task('sec_bolt::disable_win_user', disable_user => dproberts, $win_nodes)
+  run_task('sec_bolt::disable_win_user', $win_nodes)
   #Remove Bob's admin rights
   #run_task('sec_bolt::remove_admin_win', user => 'bob', $win_nodes)
   run_task('sec_bolt::remove_admin_win', $win_nodes)
