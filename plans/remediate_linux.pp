f6b96f9eaa80bb23462021ebdb0bb55be51a4c30
Dan Shauver <dan.shauver@puppet.com>
2019-06-25 10:38:56 -0500

    Fixing error in remediate_linux.pp

--- plans/remediate_linux.pp
+++ plans/remediate_linux.pp
@@ -2,7 +2,7 @@ plan sec_bolt::remediate_linux(
   TargetSpec $nodes,
 ) {
     run_plan(facts, nodes => $nodes)
-    $centos_nodes = get_targets($nodes).filter |$n| { $n.facts['os']['name'] == 'CentOS'
+    $centos_nodes = get_targets($nodes).filter |$n| { $n.facts['os']['name'] == 'CentOS' }
 
     run_task('sec_bolt::disable_nix_user', $centos_nodes)
     run_task('sec_bolt::remove_admin_nix', $centos_nodes)
